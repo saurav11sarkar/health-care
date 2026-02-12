446e3fe03863671287eb18d202ba0615bc37c46d
saurav11sarkar <sarkarsaurav11@outlook.com>
2026-02-12 14:55:01 +0600

    Implement global error handling with MiddlewaresFilter; add AppError class for structured error responses; update main.ts to use global filters

--- src/app/middlewares/globalerror.filter.ts
+++ src/app/middlewares/globalerror.filter.ts
@@ -63,3 +63,81 @@
 //     });
 //   }
 // }
+
+//==================================================================================
+
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { handleDuplicateError } from '../errors/handle-duplicate-error';
+import { Prisma } from 'src/generated/prisma/client';
+import AppError from '../errors/appError';
+
+@Catch()
+export class MiddlewaresFilter<T> implements ExceptionFilter {
+  catch(exception: T, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    let statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = 'Something went wrong!';
+    let errorSources = [{ path: '', message: 'Something went wrong' }];
+
+    if (exception instanceof Prisma.PrismaClientKnownRequestError) {
+      if (exception.code === 'P2002') {
+        statusCode = HttpStatus.BAD_REQUEST;
+        const constraint = (exception.meta as any)?.driverAdapterError?.cause
+          ?.constraint;
+        message = `duplicate field error: ${constraint.fields[0]}`;
+
+        errorSources = constraint;
+      }
+      if (exception.code === 'P2003') {
+        statusCode = HttpStatus.BAD_REQUEST;
+        message = 'Foreign key constraint failed';
+        const constraint = (exception.meta as any)?.driverAdapterError?.cause
+          ?.constraint;
+        errorSources = constraint;
+      }
+      if (exception.code === 'P1000') {
+        statusCode = HttpStatus.BAD_REQUEST;
+        message = 'Authentication failed aginest database server';
+        const constraint = (exception.meta as any)?.driverAdapterError?.cause
+          ?.constraint;
+        errorSources = constraint;
+      }
+    } else if ((exception as any)?.code === 11000) {
+      const simplified = handleDuplicateError(exception);
+      statusCode = simplified.statusCode;
+      message = simplified.message;
+      errorSources = simplified.errorSources;
+    } else if (exception instanceof AppError) {
+      statusCode = exception.statusCode;
+      message = exception.message;
+      errorSources = [{ path: '', message }];
+    } else if (exception instanceof HttpException) {
+      statusCode = exception.getStatus();
+      message = exception.message;
+      errorSources = [
+        {
+          path: '',
+          message,
+        },
+      ];
+    }
+
+    response.status(statusCode).json({
+      success: false,
+      statusCode,
+      path: request.url,
+      message,
+      errorSources,
+    });
+  }
+}
